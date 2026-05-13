9f76fb6f3a593395d321b5b79883c20a89d79476
ruanmolinari1898-coder <ruanmolinari1898@gmail.com>
2026-05-12 21:23:39 -0300

    Inicializa repositório

---
+++ aula12/comandos.py
@@ -0,0 +1,10 @@
+# python -m venv venv
+# source ./venv/Scripts/activate
+# Instalar as bibliotecas:
+# pip install pandas...
+# deactivate
+# rm -rf venv
+
+# Usar o arquivo requirements para fazer a instalação das bibliotecas
+# python -m venv venv
+# pip install -r requirements.txt
